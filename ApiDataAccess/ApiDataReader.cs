19eb02480cd8e06f522fd105d8ca06e14c9a2c58
VladimirFomin90 <VladimirFomin90@yandex.ru>
2024-07-02 00:00:08 +0500

    Init Project

---
+++ ApiDataAccess/ApiDataReader.cs
@@ -0,0 +1,17 @@
+public interface IApiDataReader
+{
+    Task<string> Read(string baseAddress, string requestUri);
+}
+
+public class ApiDataReader : IApiDataReader
+{
+    public async Task<string> Read(string baseAddress, string requestUri)
+    {
+        using var client = new HttpClient();
+        client.BaseAddress = new Uri(baseAddress);
+        var response = await client.GetAsync(requestUri);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
