6175804eb99805ce653a0b96a9825bce82df79a0
VladimirFomin90 <VladimirFomin90@yandex.ru>
2024-07-04 23:48:41 +0500

    DTO

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿using System.Text.Json;
-using System.Text.Json.Serialization;
 using StarWarsPlanetsStats.ApiDataAccess;
+using StarWarsPlanetsStats.DTO;
 
+// string json = null;
+
+try
+{
 IApiDataReader apiDataReader = new ApiDataReader();
+// var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
 var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
+}
+catch (HttpRequestException e)
+{
+    Console.WriteLine("API request was unsuccessful" + e.Message);
+    throw;
+}
 
 var root = JsonSerializer.Deserialize<Root>(json);
 
@@ -11,27 +22,3 @@ Console.WriteLine("Press any key to close");
 Console.ReadKey();
 
 // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
-public record Result(
-    [property: JsonPropertyName("name")] string name,
-    [property: JsonPropertyName("rotation_period")] string rotation_period,
-    [property: JsonPropertyName("orbital_period")] string orbital_period,
-    [property: JsonPropertyName("diameter")] string diameter,
-    [property: JsonPropertyName("climate")] string climate,
-    [property: JsonPropertyName("gravity")] string gravity,
-    [property: JsonPropertyName("terrain")] string terrain,
-    [property: JsonPropertyName("surface_water")] string surface_water,
-    [property: JsonPropertyName("population")] string population,
-    [property: JsonPropertyName("residents")] IReadOnlyList<string> residents,
-    [property: JsonPropertyName("films")] IReadOnlyList<string> films,
-    [property: JsonPropertyName("created")] DateTime created,
-    [property: JsonPropertyName("edited")] DateTime edited,
-    [property: JsonPropertyName("url")] string url
-);
-
-public record Root(
-    [property: JsonPropertyName("count")] int count,
-    [property: JsonPropertyName("next")] string next,
-    [property: JsonPropertyName("previous")] object previous,
-    [property: JsonPropertyName("results")] IReadOnlyList<Result> results
-);
-
