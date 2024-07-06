7ad1b84e680f7ce352035ea9509645be1b8f2894
VladimirFomin90 <VladimirFomin90@yandex.ru>
2024-07-06 22:48:56 +0500

    global try catch

--- Program.cs
+++ Program.cs
@@ -2,23 +2,38 @@
 using StarWarsPlanetsStats.ApiDataAccess;
 using StarWarsPlanetsStats.DTO;
 
-// string json = null;
-
 try
 {
-IApiDataReader apiDataReader = new ApiDataReader();
-// var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
-var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
+    await new StarWarsPlanetsStatsApp().Run();
 }
-catch (HttpRequestException e)
+catch (Exception e)
 {
-    Console.WriteLine("API request was unsuccessful" + e.Message);
-    throw;
+    Console.WriteLine("An error has occurred" + e.Message);
 }
-
-var root = JsonSerializer.Deserialize<Root>(json);
 
 Console.WriteLine("Press any key to close");
 Console.ReadKey();
+
+public class StarWarsPlanetsStatsApp
+{
+    public async Task Run()
+    {
+        string json = null;
+
+        try
+        {
+            IApiDataReader apiDataReader = new ApiDataReader();
+// var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
+            var json = await apiDataReader.Read("https://swapi.dev/", "api/planets");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("API request was unsuccessful" + e.Message);
+            throw;
+        }
+
+        var root = JsonSerializer.Deserialize<Root>(json);
+    }
+}
 
 // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
